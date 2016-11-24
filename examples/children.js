34136a19c437cc55de5989763195fab6244dda61
Mateusz Mazurek <matt@scalify.co>
2016-11-24 18:11:11 +0100

    example of inserting node to description

---
+++ examples/children.js
@@ -0,0 +1,18 @@
+require('rc-steps/assets/index.less');
+require('rc-steps/assets/iconfont.less');
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const { Steps, Step } = require('rc-steps');
+
+const container = document.getElementById('__react-content');
+
+ReactDOM.render(
+  <Steps direction="vertical" current={2}>
+    <Step title="Test 1" />
+    <Step title="Second">
+      <h1>Test</h1> 2
+    </Step>
+    <Step title="Third" description="Test 3" />
+  </Steps>
+, container);
